40f41da6d1dc0d277b0a71312e5f2e5dee166d1b
Greg Mundy <gregmundy@gmail.com>
2016-06-17 10:43:42 -0400

    small modification to onet sklls endpoint to return all element ids for a given onet soc code

--- api/skills.py
+++ api/skills.py
@@ -17,12 +17,17 @@ import json
 class ONETSkill(Resource):
     def get(self, id):
         if id is not None:
-            output = {}
-            result = SkillsMaster.query.filter_by(onet_soc_code = id).first()
+            output_array = []
+            result = SkillsMaster.query.filter_by(onet_soc_code = id).all()
             if result is not None:
-                output['skill_uuid'] = result.skill_uuid
-                output['onet_soc_code'] = result.onet_soc_code
-                return output
+                for res in result:
+                    output = {}
+                    output['skill_uuid'] = res.skill_uuid
+                    output['onet_soc_code'] = res.onet_soc_code
+                    output['onet_element_id'] = res.onet_element_id
+                    output['skill_name'] = res.skill_name
+                    output_array.append(output)
+                return output_array
         else:
             abort(404)
 
