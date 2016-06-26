82614c01ad6136951342a83eb42e9c848d8d97d0
Greg Mundy <gregmundy@gmail.com>
2016-06-25 22:44:59 -0400

    added requirements.txt app.py and skeleton for v1 of the API

---
+++ app/app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+app.app
+~~~~~~~
+
+OpenSkills API application root.
+
+"""
+
+from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_restful import Api
+
+app = Flask(__name__)
+app.config.from_object(config.config.Config)
+
+api = Api(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
