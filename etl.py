83bedead12f0689a258ed006e495cd4abb5a46eb
Greg Mundy <gregmundy@gmail.com>
2016-06-26 07:39:56 -0400

    added database migration plumbing

--- etl.py
+++ etl.py
@@ -17,5 +17,8 @@ from flask_migrate import MigrateCommand
 
 manager = Manager(app, with_default_commands=False)
 
+# Add the SQLAlchemy migration utility
+manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
     manager.run()
