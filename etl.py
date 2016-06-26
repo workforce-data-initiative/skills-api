f4acfe11823974d61ba80e71ef110dead1c4b8bc
Greg Mundy <gregmundy@gmail.com>
2016-06-26 07:35:33 -0400

    created initial code stub for extract-transform-load utility

---
+++ etl.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+etl.py
+~~~~~~
+OpenSkills API Extract-Transform-Load utility
+
+"""
+
+import os
+import hashlib
+import uuid
+
+from app.app import app, db
+from flask_script import Manager
+from flask_migrate import MigrateCommand
+
+manager = Manager(app, with_default_commands=False)
+
+if __name__ == '__main__':
+    manager.run()
