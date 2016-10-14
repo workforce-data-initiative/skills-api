386042d90bf2b9160cfdd572cb325eb2b6c50a67
Tristan Crockett <tristan.h.crockett@gmail.com>
2016-10-14 18:15:26 -0500

    Add tox, fix job normalizer test

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Skills API',
+    version='1.0',
+    description='Skills API',
+    packages=find_packages(),
+)
